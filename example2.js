3b6c3e7b911b5bc17b39806d191d57d44f180d4c
Stately World <47112910+statelyworld@users.noreply.github.com>
2019-05-25 15:13:38 +0530

    Create example2.js

---
+++ example2.js
@@ -0,0 +1,7 @@
+$(this).hide() // hides the current element.
+
+$("p").hide() // hides all <p> elements.
+
+$(".test").hide() // hides all elements with class="test".
+
+$("#test").hide() // hides the element with id="test".
